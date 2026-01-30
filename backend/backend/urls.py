b4ebab2c4ace3ba8fd0fdde66589a14922b92afe
Sujal Kumar <sujalkumarsahu24@gmail.com>
2026-01-30 12:49:18 +0530

    idk

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
+    path('/', include('api.urls')),
     path('admin/', admin.site.urls),
     path('api/user/register/', CreateUserView.as_view(), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
